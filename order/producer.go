a98f88fc9ec630046c80efcc9ae74e3aed84ab6b
rasadov <raufasadov23@gmail.com>
2025-04-05 02:09:03 +0400

    fix of go routine handling mechanism in order service

--- order/producer.go
+++ order/producer.go
@@ -16,6 +16,8 @@ type Event struct {
 	EventData EventData `json:"data"`
 }
 
+var done = make(chan bool)
+
 func (service orderService) SendMessageToRecommender(event Event, topic string) error {
 	jsonMessage, err := json.Marshal(event)
 	if err != nil {
@@ -28,18 +30,32 @@ func (service orderService) SendMessageToRecommender(event Event, topic string) 
 		Value: sarama.StringEncoder(jsonMessage),
 	}
 
+	// Send the message asynchronously
 	service.producer.Input() <- msg
 
+	return nil
+}
+
+func (service orderService) MsgHandler() {
 	go func() {
 		for {
 			select {
 			case success := <-service.producer.Successes():
 				log.Printf("Message sent to partition %d at offset %d\n", success.Partition, success.Offset)
-			case err = <-service.producer.Errors():
+			case err := <-service.producer.Errors():
 				log.Printf("Failed to send message: %v\n", err)
+			case <-done:
+				log.Println("Producer closed successfully")
+				return
 			}
 		}
 	}()
+}
 
-	return nil
+func (service orderService) Close() {
+	if err := service.producer.Close(); err != nil {
+		log.Printf("Failed to close producer: %v\n", err)
+	} else {
+		done <- true
+	}
 }
